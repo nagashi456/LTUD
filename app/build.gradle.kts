e09002e5b47d62ba270d8ea7fea380112bb13db7
thucuyenne <thucuyen160804@gmail.com>
2025-04-04 15:34:35 +0700

    hai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "org.o7planning.a48k141_03_duan_quanlyhoctap"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.o7planning.a48k141_03_duan_quanlyhoctap"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/org/o7planning/a48k141_03_duan_quanlyhoctap/MainActivity.java
+++ app/src/main/java/org/o7planning/a48k141_03_duan_quanlyhoctap/MainActivity.java
@@ -2,23 +2,45 @@ package org.o7planning.a48k141_03_duan_quanlyhoctap;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final Fragment homeFragment = new HomeFragment();
+    private final Fragment chatFragment = new ChatFragment();
+    private final Fragment notificationFragment = new NotificationFragment();
+    private final Fragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        setCurrentFragment(homeFragment);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                setCurrentFragment(homeFragment);
+            } else if (itemId == R.id.chat) {
+                setCurrentFragment(chatFragment);
+            } else if (itemId == R.id.notification) {
+                setCurrentFragment(notificationFragment);
+            } else if (itemId == R.id.profile) {
+                setCurrentFragment(profileFragment);
+            }
+            return true;
         });
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .commit();
     }
 }
